5b26890d8c88501190d4198e3824c247bda52b1c
Dru <drurly@gmail.com>
2013-02-09 08:40:41 -0500

    Change time of time

---
+++ db/migrate/20130209133837_change_typeof_time.rb
@@ -0,0 +1,5 @@
+class ChangeTypeofTime < ActiveRecord::Migration
+  def change
+    change_column :events, :time, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209010104) do
+ActiveRecord::Schema.define(:version => 20130209133837) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(:version => 20130209010104) do
     t.string   "host"
     t.string   "date"
     t.string   "location"
-    t.integer  "time"
+    t.string   "time"
     t.string   "phone_number"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
