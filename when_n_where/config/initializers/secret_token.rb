2cac46e9449e6f7d7961e2086f80ba14ec7a22cf
Dru <drurly@gmail.com>
2013-02-08 19:33:46 -0500

    First commit

---
+++ when_n_where/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhenNWhere::Application.config.secret_token = 'd15ea0bedb725c86da8c4522a60ee2dab8e3e868a6fec518e69ef4338e558093e00f6fd8d0b189e91b2a7192ca574bd2d1c1ff68f005651993f0c1faf1e00d55'
---
+++ when_n_where/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WhenNWhere::Application.config.session_store :cookie_store, key: '_when_n_where_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WhenNWhere::Application.config.session_store :active_record_store
