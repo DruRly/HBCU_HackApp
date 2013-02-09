3e77243db299a10cab4171cf5b21b3ad33f2715b
Dru <drurly@gmail.com>
2013-02-08 21:41:12 -0500

    Basic Workflow

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,9 +4,19 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def create
+    @event = Event.new(params[:event])
+    if @event.save
+      redirect_to action: :index
+    else
+      redirect_to events_path
+    end
+  end
 
+  def new
+    @event = Event.new
   end
 end
